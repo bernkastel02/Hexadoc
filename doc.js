fd75be5f7553acabeedccacea6151c08b90cd0aa
Jack <epicawesomewolfgaming@gmail.com>
2016-10-16 16:56:48 -0400

    eval cmd for mee

--- doc.js
+++ doc.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const fileCount = require("file-count");
 const bot = new Discord.Client();
 const fs = require("fs");
+const ownerID = "153244623219851266"
 var prefix = "hexa, ";
 
 //this disgusts me
@@ -33,7 +34,38 @@ bot.on("message", message => {
   if(message.content === `${prefix}help`) {
     message.channel.sendMessage("Hello! My name is Hexadoc, and I am here to serve you! My current commands are `docs`, followed by the document you want to view, along with `doclist` to view the overall. \n Created by jack ✗#5403")
   }
+  if(message.content.startsWith(`${prefix}eval`)) {
+    if(message.author.id == (ownerID)) {
+      var code = message.content.split(" ").slice(1).join(" ");
+      console.log("User has evaluated something!".green)
+      message.channel.sendMessage(":diamond_shape_with_a_dot_inside: | **Hexadoc Evaluator");
+      message.channel.sendMessage("Input ```xl\n" +
+      code +
+      "\n```"
+      );
+
+      try {
+          var evaled = eval(code);
+          if (typeof evaled !== 'string')
+            evaled = require('util').inspect(evaled);
+          msg.channel.sendMessage("Output```xl\n" +
+          clean(evaled) +
+          "\n```" +
+          "Evaluation Complete! ✅"
+          );
+      }
+      catch(err) {
+          msg.channel.sendMessage("🚫 `ERROR` ```xl\n" +
+          clean(err) +
+          "\n```");
+      }
+    }
+  })
+    }
+  }
 });
+
+
 
 
 bot.login('token');
